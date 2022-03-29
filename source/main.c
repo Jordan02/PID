91f92c3076c93525b9fe3a6024696e3f77146158
jordan02 <mills667jordan@gmail.com>
2022-03-29 20:56:19 +0100

    init

---
+++ source/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+  {
+
+
+    printf("helloWorld");
+    int num =10;
+    
+    //printf("enter a number faggot:");
+    //scanf("%d", num );
+    //printf("your number was %d faggor man", num);
+
+
+    return 1; 
+}
